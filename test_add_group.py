f48b0abcc9e95285f3240bd6eb3bd17c4b97360b
a_kukushkin <flight-85@mail.ru>
2024-10-21 18:45:49 +0300

    Сделал всё с нуля в Firefox

--- test_add_group.py
+++ test_add_group.py
@@ -14,7 +14,7 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -22,18 +22,19 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("258")
+        wd.find_element_by_name("group_name").send_keys("new_group_firefox")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("mklsd;ghsa")
+        wd.find_element_by_name("group_header").send_keys("jhklpjhlksdjal;fkj")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("sdfg,lagp;sal")
+        wd.find_element_by_name("group_footer").send_keys(u"полфкджплкпмдкфж")
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("groups").click()
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
@@ -45,6 +46,7 @@ class TestAddGroup(unittest.TestCase):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
+    
 
     def tearDown(self):
         self.wd.quit()
