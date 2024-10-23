fd30dd5c3e3f169cec701b6e410c65a0404dafdf
a_kukushkin <flight-85@mail.ru>
2024-10-23 03:15:42 +0300

    Подключил вспомогательный класс Contsct для инициализации тестовых данных в методе добавления нового контакта. Тест отрабатывает как из консоли так и из PyCharm

--- contact.py
+++ contact.py
@@ -1,5 +1,6 @@
+
 class Contact:
-    def __int__(self, firstname, midlename, lastname, nickname, tittle, company, address, home, mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear):
+    def __init__(self, firstname, midlename, lastname, nickname, tittle, company, address, home, mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, bmonth_x, aday_x, amonth_x):
         self.firstname = firstname
         self.midlename = midlename
         self.lastname = lastname
@@ -14,11 +15,14 @@ class Contact:
         self.email = email
         self.email2 = email2
         self.email3 = email3
-        self.homapage = homepage
+        self.homepage = homepage
         self.bday = bday
         self.bmonth = bmonth
         self.byear = byear
         self.aday = aday
         self.amonth = amonth
         self.ayear = ayear
+        self.bmonth_x = bmonth_x
+        self.aday_x = aday_x
+        self.amonth_x = amonth_x
 
