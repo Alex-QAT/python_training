577d01748b87fb2d066461e73667d5691678c8dc
a_kukushkin <flight-85@mail.ru>
2024-10-30 01:23:02 +0300

    - Добавлены тесты по операции удаления контакта, редактированию группы, редактированию контакта ============================ Ранее в предыдущем комите реализовано: - Создан тест по операции удаления группы - Инициализация фикстуры для всех тестов вынесена в общий файл conftest.py - Фикстура доработана чтобы все тесты выполнялись в одной сессии вебдрайвера (браузера) - Доработан метод logout: теперь после logout мы проверяем наличие на странице элемента поле ввода user и только после этого переходим к выплнению следующего теста, потому что была проблема, следующий тест с метода login начинался раньше, чем появится форма для login.

--- fixture/group.py
+++ fixture/group.py
@@ -30,6 +30,26 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
+    def edit_1st_gr(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # init group edition
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group edition
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
     def del_1st_gr(self):
         wd = self.app.wd
         self.open_group_page()
