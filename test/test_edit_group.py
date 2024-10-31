357f5f3459a9e27299b022a0af27ee073d6eacde
a_kukushkin <flight-85@mail.ru>
2024-11-01 00:48:18 +0300

    исправил название метода в файле test_edit_group был копипаст test_add_group стал test_edit_1st_group ни на что не влияет, но чтоб не было путаницы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,7 @@
 from model.group import Group
 
 
-def test_add_group(app):
+def test_edit_1st_group(app):
     app.session.login("admin", "secret")
     app.group.edit_1st(Group("!Начальники", "что-то написать", "что-то шрлмавлд"))
     app.session.logout()
