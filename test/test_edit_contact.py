cd62e00aea2ae7dd9dea3c3ea2ff38e626fce3b2
a_kukushkin <flight-85@mail.ru>
2024-11-10 21:38:50 +0300

    - в фикстуре GroupHelper добавлен метод получения списка групп (извлекаем name и id групп) - в фикстуре ContactHelper добавлен метод получения списка  контактов (извлекаем lastname, firstname и id контакта) - в тесте add_group добавлено сравнение длинны списка групп до и после добавления (Новый список на 1 больше чем старый). - в тесте del_group добавлено сравнение длинны списка групп до и после удаления (Новый список на 1 меньше чем старый). - в тесте edit_group добавлено сравнение длинны списка групп до и после удаления (Новый список равен старому). - Аналогично сделано для тестов добавления, удаления и редактирования Контактов.

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -4,24 +4,36 @@ from model.contact import Contact
 def test_edit_1st_con(app):
     if app.contact.count_con() == 0:
         app.contact.add_new(Contact(firstname="Петя"))
+    old_con = app.contact.get_con_list()
     app.contact.edit_1st(Contact(u"Вася", u"Иванович", u"Васильев", "Peter",
                              u"Начальник отдела продаж", u"Grass", u"Волгоград, Аллея героев 23",
                              "+7-495-536-18-27", "+7-917-568-44-45", "+7-988-332-11-25", "4-05-18", "peterpen@mail.ru",
                              "peterpen2@mail.ru", "peterpen3@mail.ru", "www.vk.com/peter_1st", "12", "September", "1981",
                              "12", "January", "2025"))
+    new_con = app.contact.get_con_list()
+    assert len(old_con) == len(new_con)
 
 
 def test_edit_1st_con_firstname(app):
     if app.contact.count_con() == 0:
         app.contact.add_new(Contact(firstname="Петя"))
+    old_con = app.contact.get_con_list()
     app.contact.edit_1st(Contact(firstname="Сергей"))
+    new_con = app.contact.get_con_list()
+    assert len(old_con) == len(new_con)
 
 def test_edit_1st_con_mdlname(app):
     if app.contact.count_con() == 0:
         app.contact.add_new(Contact(midlename="Петрович"))
+    old_con = app.contact.get_con_list()
     app.contact.edit_1st(Contact(midlename="Михалыч"))
+    new_con = app.contact.get_con_list()
+    assert len(old_con) == len(new_con)
 
 def test_edit_1st_con_lstname(app):
     if app.contact.count_con() == 0:
         app.contact.add_new(Contact(lastname="Сидоров"))
+    old_con = app.contact.get_con_list()
     app.contact.edit_1st(Contact(lastname="Мустафаев"))
+    new_con = app.contact.get_con_list()
+    assert len(old_con) == len(new_con)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,22 +4,34 @@ from model.group import Group
 def test_edit_1st_group_full(app):
     if app.group.count_gr() == 0:
         app.group.create(Group("test-group"))
+    old_groups = app.group.get_gr_list()
     app.group.edit_1st(Group(name="!Начальники", header="что-то написать", footer="что-то шрлмавлд"))
+    new_groups = app.group.get_gr_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_1st_group_name(app):
     if app.group.count_gr() == 0:
         app.group.create(Group("test-group"))
+    old_groups = app.group.get_gr_list()
     app.group.edit_1st(Group(name="New Name_Group" ))
+    new_groups = app.group.get_gr_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_1st_group_header(app):
     if app.group.count_gr() == 0:
         app.group.create(Group("test-group"))
+    old_groups = app.group.get_gr_list()
     app.group.edit_1st(Group(header="New Header"))
+    new_groups = app.group.get_gr_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_1st_group_footer(app):
     if app.group.count_gr() == 0:
         app.group.create(Group("test-group"))
+    old_groups = app.group.get_gr_list()
     app.group.edit_1st(Group(footer="New Footer"))
+    new_groups = app.group.get_gr_list()
+    assert len(old_groups) == len(new_groups)
